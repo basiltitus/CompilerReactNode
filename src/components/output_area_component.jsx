0026c9fa1c63f30ce2391ab3854899fc110f4584
emilsharier <emil@navigus.in>
2020-11-08 18:09:52 +0530

    Fixed output field

--- src/components/output_area_component.jsx
+++ src/components/output_area_component.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class OutputArea extends Component{
-    render(){
-        return<div><br/> Output :
-            <textarea rows={2} cols={50}></textarea>
-        </div>
-    }
+export default class OutputArea extends Component {
+  render() {
+    return (
+      <div>
+        <br /> Output :
+        <textarea rows={2} cols={50} value={this.props.output}>
+          {this.props.output}
+        </textarea>
+      </div>
+    );
+  }
 }
